53cd96ceb9e5317475f4af7e0ad22e6838f8aaa7
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2024-05-05 17:13:13 +0200

    Update bug_algorithm.py

--- assignment_pkg/scripts/bug_algorithm.py
+++ assignment_pkg/scripts/bug_algorithm.py
@@ -10,15 +10,10 @@ def angle_to_target(position, quaternion):
     return angle_to_target
 
 def bugAlgorithm():
-    # Implementazione dell'algoritmo Bug
-    prevoius_state = ''
     current_state = 'align_robot_heading'
-    theta_threshold = 4 
+    theta_threshold = 4.0 
+    threshold_distance = 5.0
     sensor_noise = 0.82 # distance sensor of airsim has a gaussian noise and this value helps in reduce te noise impact on decision
-    threshold_distance_obstacle = 5.0
-
-    env.client.simGetCollisionInfo().has_collided
-    start = time.time()
 
     while True:
         sensor_data = client.getDistanceSensorData(distance_sensor_name = '', vehicle_name = '').distance
@@ -27,38 +22,27 @@ def bugAlgorithm():
 
         # Aligns the heading angle of the quad to the target goal position.
         if current_state == 'align_robot_heading':
-            
             angle_target = angle_to_target(position, quaternion)
+
             if theta_threshold > min(angle_target, 360-angle_target):
-                prevoius_state = current_state
                 current_state = 'move_to_goal'
             else: # robot heading is not aligned, thus rotate by yaw
                 if angle_target <= 180:
                     action = 4 # rotate right
                 else:
                     action = 5 # rotate left
-
-        if current_state == 'move_to_goal':
+        elif current_state == 'move_to_goal':
             action = 0
-            front_sensor = env.client.getDistanceSensorData('Distance_0') # oppure .getDistanceSensorData('Distance_0').distance
-            distance = front_sensor_data.max_distance - front_sensor_data.min_distance - sensor_noise
+            distance = sensor_data.max_distance - sensor_data.min_distance - sensor_noise
 
             if distance < threshold_distance:
-                prevoius_state = current_state
                 current_state = 'wall'
 
+        solved = env.step(action, is_rate, angle_value)
+        time.sleep(0.25) # delay for correct movement
 
-    solved = env.step(action, is_rate, angle_value)
-    time.sleep(0.25) # delay for correct movement
-
-    if solved:
-        # Write log of training
-        elapsed_time = abs(round(time.time() - start, 2))
-        minutes = int(elapsed_time // 60)
-        seconds = int(round(elapsed_time % 60, 2))
-        result = (f'Navigation Done. Time: {minutes}:{seconds}\n')
-        print(result)
-        return
+        if solved:
+            return
 
 
 def main():
