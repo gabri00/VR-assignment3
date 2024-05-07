2870e1a5373eed40084ff0d7cc0e9a471f8064c6
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2024-05-07 12:19:22 +0200

    ripristino parziale di bug

--- assignment_pkg/scripts/bug_algorithm.py
+++ assignment_pkg/scripts/bug_algorithm.py
@@ -3,60 +3,82 @@
 import rospy
 import airsim
 import time
+import numpy as np
 from Environment import DroneEnvironment
 
-def angle_to_target(position, quaternion):
-    angle_to_target = env.calculate_angle(position, quaternion, for_Mline=True)
-    return angle_to_target
 
-def bugAlgorithm():
-    current_state = 'align_robot_heading'
-    theta_threshold = 4.0 
-    threshold_distance = 5.0
-    sensor_noise = 0.82 # distance sensor of airsim has a gaussian noise and this value helps in reduce te noise impact on decision
+class Bug2:
+    def __init__(self):
+        # Initialize AirSim client
+        self.host = rospy.get_param('~host')
+        self.client = airsim.MultirotorClient(ip=self.host, port=41451)
+        self.client.confirmConnection()
+        
+        self.env = DroneEnvironment(self.client)
 
-    while True:
-        sensor_data = client.getDistanceSensorData(distance_sensor_name = '', vehicle_name = '').distance
-        position, quaternion = env.current_position() # Retrieve current position & quaternion (orientation) data
-        is_rate, angle_value = True, 0
+        self.m_line = self.env.goal - self.env.initial_position
+        self.m_line = self.m_line / np.linalg.norm(self.m_line)
 
-        # Aligns the heading angle of the quad to the target goal position.
-        if current_state == 'align_robot_heading':
-            angle_target = angle_to_target(position, quaternion)
+    
+    def angle_to_target(self, position, quaternion):
+        return self.env.calculate_angle(position, quaternion, for_Mline=True)
 
-            if theta_threshold > min(angle_target, 360-angle_target):
-                current_state = 'move_to_goal'
-            else: # robot heading is not aligned, thus rotate by yaw
-                if angle_target <= 180:
-                    action = 4 # rotate right
-                else:
-                    action = 5 # rotate left
-        elif current_state == 'move_to_goal':
-            action = 0
-            distance = sensor_data.max_distance - sensor_data.min_distance - sensor_noise
 
-            if distance < threshold_distance:
-                current_state = 'wall'
+    def dist_to_Mline(self, quad_position):
+        quad_to_init_vector = quad_position - self.env.initial_position
 
-        solved = env.step(action, is_rate, angle_value)
-        time.sleep(0.25) # delay for correct movement
+        # Vector from quad position to M-line (Projection vector of the quad's position onto the M-line.)
+        quad_to_mline = np.dot(quad_to_init_vector, self.m_line) * self.m_line
 
-        if solved:
-            return
+        # Distance to M-line
+        return np.linalg.norm(quad_position - (self.env.initial_position + quad_to_mline))
+
+
+    def run(self):
+        prev_state = 'start'
+        curr_state = 'align_robot_heading'
+
+        theta_threshold = 4.0 
+        dist_threshold = 5.0
+        dist_to_Mline_threshold = 0.3
+        
+        sensor_noise = 0.82 # distance sensor of airsim has a gaussian noise and this value helps in reduce te noise impact on decision
+
+        while True:
+            sensor_data = self.client.getDistanceSensorData(distance_sensor_name = '', vehicle_name = '').distance
+            position, quaternion = self.env.current_position() # Retrieve current position & quaternion (orientation) data
+            is_rate, angle_value = True, 0
+
+            # Align drone to target
+            if curr_state == 'align_robot_heading':
+                angle_target = self.angle_to_target(position, quaternion)
+
+                if theta_threshold > min(angle_target, 360-angle_target):
+                    prev_state = curr_state
+                    curr_state = 'move_to_goal'
+                else: # robot heading is not aligned, thus rotate by yaw
+                    if angle_target <= 180:
+                        action = 4 # rotate right
+                    else:
+                        action = 5 # rotate left
+            elif curr_state == 'move_to_goal':
+                action = 0
+
+            elif curr_state == 'turn':
+                is_rate, angle_value = False, 90
+
+            solved = self.env.step(action, is_rate, angle_value)
+            time.sleep(0.25) # delay for correct movement
+
+            if solved:
+                return
 
 
 def main():
-    global env, client
-
     rospy.init_node('bug_node')
 
-    # Initialize AirSim client
-    host = rospy.get_param('~host')
-    client = airsim.MultirotorClient(ip=host, port=41451)
-    client.confirmConnection()
-
-    env = DroneEnvironment(client)
-    bugAlgorithm()
+    bug = Bug2()
+    bug.run()
 
     rospy.spin()
 
