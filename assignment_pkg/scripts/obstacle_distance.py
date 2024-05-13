f443d03b17dd19647493d97c8b2e8314b0c3b8b5
Alessio Mura <93042208+alemuraa@users.noreply.github.com>
2024-05-13 21:51:20 +0200

    Update obstacle_distance.py
    obstacle sensor node that return the distance of the drone from the obstacle (straight, right, left)

--- assignment_pkg/scripts/obstacle_distance.py
+++ assignment_pkg/scripts/obstacle_distance.py
@@ -1,27 +1,55 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Int64
 import airsim
 import time
 
+# Define the meaning of sensor codes
+# Code = 1: front sensor 
+# Code = 2: right sensor
+# Code = 3: left sensor
 
 def check_obstacles(event):
-	# Get current Distance data
-	distance_sensor_data = client.getDistanceSensorData(distance_sensor_name = '', vehicle_name = '')
 
+	global code
+    if code == 1:
+        distance_sensor_data = client.getDistanceSensorData(distance_sensor_name='Distance_0', vehicle_name='')
+    elif code == 2:
+        distance_sensor_data = client.getDistanceSensorData(distance_sensor_name='Distance_M90', vehicle_name='')
+    elif code == 3:
+        distance_sensor_data = client.getDistanceSensorData(distance_sensor_name='Distance_90', vehicle_name='')
+    else:
+        rospy.logwarn("Unknown sensor code: %d", code)
+        return
+
+
+	# Get current Distance data
+	distance_condition = distance_sensor_data.max_distance - distance_sensor_data.min_distance - sensor_noise
+	
 	# Publish current obstacle distance
 	rospy.loginfo('Distance from obstacle: %f', distance_sensor_data.distance)
 	distance_pub.publish(distance_sensor_data.distance)
 
+
+def sensor_code_callback(msg):
+     global code
+    rospy.loginfo('Sensor code received: %f', msg.data)
+    code = msg.data
 
 def main():
 	global client, distance_pub
 
 	rospy.init_node('distance_sensor_node')
 
+	# Initialize sensor noise
+    sensor_noise = 0.82
+
 	# Publishers
 	distance_pub = rospy.Publisher('distance_data', Float64, queue_size=10)
+
+	# Subscribers
+    rospy.Subscriber('sensor_code', Int64, sensor_code_callback)
 
 	# Initialize AirSim client
 	host = rospy.get_param('~host')
