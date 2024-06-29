684890021cc034504a1cb6029225d35cd46cfed0
Alessio Mura <93042208+alemuraa@users.noreply.github.com>
2024-06-29 15:57:23 +0200

    Update possible_battery.py

--- assignment_pkg/scripts/possible_battery.py
+++ assignment_pkg/scripts/possible_battery.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import airsim
-import time
 import numpy as np
 
-# Funzione per calcolare il consumo della batteria per unit‡ di distanza
+# Funzione per calcolare il consumo della batteria per unit√† di distanza
 def calcola_consumo_batteria_per_distanza(peso, condizioni_atmosferiche):
     return 0.05 * peso + 0.03 * condizioni_atmosferiche
 
 # Parametri
 peso_oggetto = 10  # peso dell'oggetto trasportato
 condizioni_atmosferiche = 5  # parametro relativo alle condizioni atmosferiche (es. vento, pioggia)
-batteria_iniziale = 100  # Capacit‡ iniziale della batteria
+batteria_iniziale = 100  # Capacit√† iniziale della batteria
 soglia_batteria = 20  # Soglia della batteria per il ritorno
 threshold = 5  # Soglia di distanza per l'atterraggio
 
@@ -27,10 +26,10 @@ client.takeoffAsync().join()
 client.moveToPositionAsync(0, 0, -15, 5).join()
 
 # Definizione dei punti
-goal = np.array([0, -800])
-#punto_rifornimento = np.array([0, -40])  # Punto di rifornimento definito
+goal = np.array([0, -80])
+punto_rifornimento = np.array([0, -40])  # Punto di rifornimento definito
 
-# Calcola il consumo della batteria per unit‡ di distanza
+# Calcola il consumo della batteria per unit√† di distanza
 consumo_per_distanza = calcola_consumo_batteria_per_distanza(peso_oggetto, condizioni_atmosferiche)
 
 # Calcola la distanza massima percorribile con la batteria attuale
@@ -50,12 +49,14 @@ print(f"Distanza al goal: {distanza_goal:.2f} metri")
 if distanza_goal <= distanza_massima:
     print("Batteria sufficiente per raggiungere il goal")
     # GO TO GOAL
+    client.moveToPositionAsync(float(goal[0]), float(goal[1]), -15, 5).join()
 else:
     print("Batteria NON sufficiente per raggiungere il goal")
-    # GO TO WAYPOINT
+    # GO TO punto rifornimento
+    client.moveToPositionAsync(float(punto_rifornimento[0]), float(punto_rifornimento[1]), -15, 5).join()
 
 # Attendi 2 secondi
-time.sleep(5)
+#time.sleep(5)
 
 # Atterraggio del drone
 client.landAsync().join()
